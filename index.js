5454a4adc98f8b01d567c25599a057e22cf72fa6
Matt Price <matprc246@gmail.com>
2021-04-08 16:59:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 ```js
-console.error("HALP!");
+console.error("HALP!")
 ```
 ```js
-console.log("Make a deposite.");
+console.log("Make a deposite.")
 ```
 ```js
-console.warn("Hold your horses!");
+console.warn("Hold your horses!")
 ```
